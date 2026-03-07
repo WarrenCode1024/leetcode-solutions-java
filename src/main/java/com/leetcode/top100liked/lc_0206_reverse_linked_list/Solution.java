5c36e49202e4d964eec3ca4c4b830d24a8c569dc
WarrenCode1024 <warrenwei1024@gmail.com>
2026-03-07 23:42:38 +0800

    leetcode 0206 反转链表 easy

---
+++ src/main/java/com/leetcode/top100liked/lc_0206_reverse_linked_list/Solution.java
@@ -0,0 +1,39 @@
+package com.leetcode.top100liked.lc_0206_reverse_linked_list;
+
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+
+    }
+}
