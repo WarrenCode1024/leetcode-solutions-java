7192a91f8c0c5e1180357bdf026794a56ede0bf4
WarrenCode1024 <warrenwei1024@gmail.com>
2026-03-09 00:19:00 +0800

    leetcode 0104 二叉树的最大深度 easy

---
+++ src/main/java/com/leetcode/top100liked/lc_0104_maximum_depth_of_binary_tree/Solution.java
@@ -0,0 +1,31 @@
+package com.leetcode.top100liked.lc_0104_maximum_depth_of_binary_tree;
+
+//Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
