f784ed3319b04c47f122a1aaa9eded5dd6836bac
WarrenCode1024 <warrenwei1024@gmail.com>
2026-03-09 00:06:32 +0800

    leetcode 0094 二叉树的中序遍历 easy

---
+++ src/main/java/com/leetcode/top100liked/lc_0094_binary_tree_inorder_traversal/Solution.java
@@ -0,0 +1,45 @@
+package com.leetcode.top100liked.lc_0094_binary_tree_inorder_traversal;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        traverse(root, result);
+        return result;
+    }
+
+    private void traverse(TreeNode node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+        // 1. 左
+        traverse(node.left, result);
+        // 2. 根
+        result.add(node.val);
+        // 3. 右
+        traverse(node.right, result);
+    }
+}
