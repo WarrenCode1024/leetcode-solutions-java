2642095839db05149712637b736b63514cd451c3
WarrenCode1024 <warrenwei1024@gmail.com>
2026-03-08 11:58:24 +0800

    leetcode 0234 回文链表 easy

---
+++ src/main/java/com/leetcode/top100liked/lc_0234_palindrome_linked_list/Solution.java
@@ -0,0 +1,63 @@
+package com.leetcode.top100liked.lc_0234_palindrome_linked_list;
+
+
+//  Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true; // 空链表或只有一个节点，是回文
+        }
+
+        // 步骤 1: 快慢指针找到链表的中点
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // 步骤 2: 反转后半部分链表
+        ListNode secondHalf = reverseList(slow);
+        ListNode firstHalf = head;
+
+        // 步骤 3: 比较两部分
+        while (secondHalf != null) {
+            if (firstHalf.val != secondHalf.val) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
+
+    // 反转链表的辅助函数
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null, curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+}
